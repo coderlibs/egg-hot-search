ada628f76f82451ac629075ad1bebd0d8b46e266
leiweisong <83460555@qq.com>
2024-04-27 14:45:35 +0800

    启动定时任务

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 module.exports = (app) => {
   app.beforeStart(async () => {
-    // 保证应用启动监听端口前数据已经准备好了
+    // 保证应用启动监听端口前,数据已经准备好了
     // 后续数据的更新由定时任务自动触发
     await app.runSchedule("update_cache");
   });
